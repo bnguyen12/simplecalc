38bdaed17aa8804d54cbef5aebca6be808216be6
Benny Nguyen <benny.nguyenx@gmail.com>
2018-01-11 18:14:44 -0800

    Add +, -, /, *, and % operator

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,45 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        let size = args.count
+        if args[size - 1] == "count" {
+            return size - 1
+        } else if args[size - 1] == "avg" {
+            var sum : Int = 0
+            for index in 0...(size - 2) {
+                sum += Int(args[index])!
+            }
+            return sum / (size - 1)
+        } else if args[size - 1] == "fact" {
+            var sum : Int = Int(args[0])!
+            for index in 1...(size - 2) {
+                sum *= Int(args[index])!
+            }
+            return sum
+        } else {
+            let operate : String = args[1]
+            var numOne : Int! = Int(args[0]) //so that numOne can change in the "%" case
+            let numTwo : Int! = Int(args[2])
+            switch operate {
+                case "+":
+                    return numOne + numTwo
+                case "-":
+                    return numOne - numTwo
+                case "/":
+                    return numOne / numTwo
+                case "*":
+                    return numOne * numTwo
+                case "%" where numOne < numTwo :
+                    return numOne
+                case "%" :
+                    while numOne >= numTwo {
+                        numOne = numOne - numTwo
+                    }
+                    return numOne
+                default:
+                    return 1
+            }
+        }
     }
     
     public func calculate(_ arg: String) -> Int {
